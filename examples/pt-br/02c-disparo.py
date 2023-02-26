52927d4c48d74efad07dbb86df5210e1114090cc
Rodrigo Rocha <rodrigorgs@gmail.com>
2023-02-25 21:15:05 -0300

    Add example

---
+++ examples/pt-br/02c-disparo.py
@@ -0,0 +1,44 @@
+from tupy import *
+
+class Star(Image):
+    def update(self):
+        self.x += 5
+        if self.x > 640:
+            self.x = 0
+
+class Tiro(Image):
+    def __init__(self):
+        self.path = 'ball.png'
+
+    def update(self):
+        self.y -= 15
+        if self.collides_with(inimigo):
+            inimigo.destroy()
+
+class Smiley(Image):
+    def __init__(self):
+        self.tiro = None
+
+    def update(self):
+        if input.is_key_just_down('space'):
+            self.dispara()
+        if self.tiro is not None:
+            self.tiro.update()
+            if self.tiro.y < -10:
+                self.tiro.destroy()
+                self.tiro = None
+    
+    def dispara(self):
+        self.tiro = Tiro()
+        self.tiro.x = self.x
+        self.tiro.y = self.y
+
+inimigo = Star()
+inimigo.x = 0
+inimigo.y = 50
+
+player = Smiley()
+player.x = 320
+player.y = 450
+
+run(globals())
